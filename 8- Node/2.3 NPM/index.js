3703b5d59a11b6e6a44fdcadd358e5ef47bb2b88
Fernando Dalpiaz <fernandoldalpiaz@gmail.com>
2025-07-02 13:43:59 -0300

    Files Upload
    Project 2.3 and 2.4 require you to npm intall the utilized packages.

---
+++ 8- Node/2.3 NPM/index.js
@@ -0,0 +1,12 @@
+// var generateName = require("sillyname");
+// import generateName from "sillyname";
+// var sillyname = generateName();
+
+// console.log(`My name is ${sillyname}.`);
+
+//____________________________________________
+import {randomSuperhero} from 'superheroes';
+
+var name = randomSuperhero();
+
+console.log(`I am ${name}`);
